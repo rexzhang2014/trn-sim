5ce4b87c78b36c92d249fb621018d5ae6ae0084e
rexzhang2014 <rexzhang2013@gmail.com>
2022-10-25 19:15:57 +0800

    fix run

--- src/run.py
+++ src/run.py
@@ -3,14 +3,14 @@ from trnsim.strategy import *
 
 if __name__ == '__main__' :
     # data = pd.read_csv('./data/sim_cyb_L20D50G50.csv', sep=',')
-    data = pd.read_csv('./data/sim_cyb_v3.0.csv', sep=',')
+    data = pd.read_csv('./data/strategydata.csv', sep='|')
 
     hold_days=5
     topk=1
     spare_amount=10000
 
     output1 = BuyEqualAmountTopKAndHoldTDay(
-        watching_list=data, begin='2021-10-01', end='2021-11-18', ranking_metric='score', topk=topk, verbose=1, spare_amount=spare_amount,
+        watching_list=data, begin='2020-10-01', end='2020-11-18', ranking_metric='score', topk=topk, verbose=1, spare_amount=spare_amount,
         hold_days=hold_days
     ).run()
 
